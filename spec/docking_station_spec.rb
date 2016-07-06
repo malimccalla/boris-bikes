b6230668b52e95b711d50ce3ee889399bee66675
tchungnz <tchung.nz@gmail.com>
2016-07-06 13:44:00 +0100

    exercise 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,8 @@ describe DockingStation do
   it 'responds to the method "release_bike"' do
     expect(subject).to respond_to(:release_bike)
   end
+  it 'release bike creates a new bike that is working' do
+    expect(subject.release_bike).to be_working
+  end
 
 end
