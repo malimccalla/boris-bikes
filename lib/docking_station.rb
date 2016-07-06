93d06ac88474f5f12e49b018a67fe1cda45abdf2
tchungnz <tchung.nz@gmail.com>
2016-07-06 15:47:09 +0100

    modified docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,21 @@
 class DockingStation
 
+attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail 'no bikes available' unless @bike
+    @bike
   end
 
+  def dock(bike)
+    fail 'docking station is full' if @bike
+    @bike = bike
+  end
+
+=begin #same as attr_reader code
+  def bike
+    @bike
+  end
+
+=end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,36 @@
 require 'docking_station'
 
 describe DockingStation do
-
   it 'responds to the method "release_bike"' do
     expect(subject).to respond_to(:release_bike)
   end
-  it 'release bike creates a new bike that is working' do
-    expect(subject.release_bike).to be_working
+    it 'responds to the method dock with 1 argument' do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+    it 'responds to bike' do
+    is_expected.to respond_to(:bike)
+  end
+
+  describe '#release_bike' do
+      it 'returns an error when there are no bikes to release' do
+        expect {subject.release_bike}.to raise_error 'no bikes available'
+      end
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
+  end
+
+  describe '#dock(bike)' do
+    it 'returns an error when the docking station contains a bike' do
+      subject.dock(Bike.new)
+      expect {subject.dock(Bike.new)}.to raise_error 'docking station is full'
+    end
   end
 
 end
