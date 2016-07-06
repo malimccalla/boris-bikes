5292fd3725ba51a13cb5e7d865c3b399c73f9f55
Mali Michael <malimccalla@gmail.com>
2016-07-06 20:03:55 +0100

    Refactor docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,7 @@ describe DockingStation do
 
   describe '#bikes' do
 
-    it 'responds to bike' do
-      expect(subject).to respond_to(:bikes)
-    end
+    it { is_expected.to respond_to(:bikes) }
 
   end
 
@@ -22,7 +20,7 @@ describe DockingStation do
       expect { subject.release_bike }.to raise_error("No bikes available")
     end
 
-    it 'responds to the method "release_bike"' do
+    it 'responds to #release_bike' do
       expect(subject).to respond_to(:release_bike)
     end
 
