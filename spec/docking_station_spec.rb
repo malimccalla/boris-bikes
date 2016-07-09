48a3e4a88bb37275bbc5bb147c77f084c6c043d5
Mali Michael <malimccalla@gmail.com>
2016-07-09 17:38:12 +0100

    Refactor docking station spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,8 +6,7 @@ describe DockingStation do
   describe '#initialize' do
 
     it 'Instantiates with a deafult capacity' do
-      docking = DockingStation.new
-      expect(docking.capacity).to eq DockingStation::DEFAULT_CAPACITY
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
     end
 
   end
@@ -23,8 +22,9 @@ describe DockingStation do
 
   describe '#release_bike' do
 
+    let (:bike) {Bike.new}
+
     it 'it wont release a broken bike' do
-      bike = Bike.new
       bike.report_broken
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error 'No working bikes'
@@ -35,7 +35,6 @@ describe DockingStation do
     end
 
     it 'only releases a bike if it is working' do
-      bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
